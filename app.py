f1ed2c2e4e7c6ccb7438e98c30f1c9920208ae94
Liam Rennie <liamrennie21@gmail.com>
2023-10-24 10:48:44 -0400

    Testing my error fix

--- app.py
+++ app.py
@@ -1,67 +1,41 @@
-from flask import Flask, render_template, request
-import pandas as pd
-import os
+from flask import Flask, render_template, request, redirect, send_from_directory
 import csv
+import os
 
-app = Flask(__name__, template_folder= 'template')
+app = Flask(__name__)
 
-# Set the folder to store uploaded CSV files
+# Define the folder for storing uploaded files
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Function to sort a CSV file by a specific column in ascending order
-def sort_csv_by_column(input_file, output_file, column_index):
+# Function to sort a CSV file by a specific column
+def sort_csv(input_file, output_file, column_index, ascending=True):
     with open(input_file, 'r') as input_csvfile:
         csvreader = csv.reader(input_csvfile)
         data = list(csvreader)
     
-    data.sort(key=lambda row: row[column_index])
+    data.sort(key=lambda row: row[column_index], reverse=not ascending)
     
     with open(output_file, 'w', newline='') as output_csvfile:
         csvwriter = csv.writer(output_csvfile)
         csvwriter.writerows(data)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def upload():
     if request.method == 'POST':
         input_file = os.path.join(app.config['UPLOAD_FOLDER'], request.files['input_file'].filename)
         output_file = os.path.join(app.config['UPLOAD_FOLDER'], request.form['output_file'])
         column_index = int(request.form['column_index'])
+        ascending = request.form['ascending'] == 'ascending'
         
         request.files['input_file'].save(input_file)
-        sort_csv_by_column(input_file, output_file, column_index)
+        sort_csv(input_file, output_file, column_index, ascending)
         return redirect('/')
-        
-    return render_template('upload.html')
-
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Check if the 'file' field is in the request
-    if 'file' not in request.files:
-        return "No file part"
-
-    file = request.files['file']
-
-    # Check if the file is not empty
-    if file.filename == '':
-        return "No selected file"
-
-    # Check if the file is allowed (e.g., has a .csv extension)
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-
-        # Read the uploaded CSV file using pandas
-        df = pd.read_csv(filename)
-
-        # Render a template to display the data
-        return render_template('display.html', data=df.to_html())
-        
     
+    return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
 def download_file(filename):
@@ -69,4 +43,3 @@ def download_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True)
-

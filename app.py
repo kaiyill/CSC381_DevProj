854ec35c9512a6c2c2ffed6c367a41056c176436
Liam Rennie <liamrennie21@gmail.com>
2023-10-25 18:29:32 -0400

    Testing standarize

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, send_from_directory, session
 import csv
 import os
+import pandas as pd
+from scipy import stats
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with a secure secret key
@@ -34,7 +36,28 @@ def sort_csv(input_file, column_index, ascending=True, output_file=None):
     else:
         return data
 
+# Function to standardize a column and handle outliers
+def standardize_column(data, column_name, method, lower_bound, upper_bound):
+    # Select the column
+    column = data[column_name]
 
+    if method == "z-score":
+        z_scores = stats.zscore(column)
+        return z_scores
+    else:
+        if method == "0-10":
+            lower_bound, upper_bound = 0, 10
+        elif method == "1-10":
+            lower_bound, upper_bound = 1, 10
+        elif method == "0-1":
+            lower_bound, upper_bound = 0, 1
+        elif method == "-1-1":
+            lower_bound, upper_bound = -1, 1
+
+        # Clip values to the specified range and then normalize
+        clipped = column.clip(lower=lower_bound, upper=upper_bound)
+        normalized = (clipped - lower_bound) / (upper_bound - lower_bound)
+        return normalized
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -58,11 +81,21 @@ def index():
             request.files['input_file'].save(input_file)
             session['input_file_path'] = input_file  # Store the file path in the session
 
+        # Standardization method
+        standardization_method = request.form['standardization_method']
+
+        # Standardize the data
+        data = pd.read_csv(input_file)
+        columns_to_standardize = [data.columns[column_index]]
+        for column_name in columns_to_standardize:
+            standardized_data = standardize_column(data, column_name, standardization_method, 0, 1 if standardization_method == "0-1" else 10)
+            data[column_name] = standardized_data
+
         if output_file_name:  # If an output file is specified
             output_file = os.path.join(app.config['UPLOAD_FOLDER'], output_file_name)
-            output_file = sort_csv(input_file, column_index, ascending, output_file)
+            data.to_csv(output_file, index=False)
         else:  # If no output file is specified
-            sorted_data = sort_csv(input_file, column_index, ascending)
+            sorted_data = data.to_dict(orient='records')
 
     return render_template('index.html', sorted_data=sorted_data, output_file=output_file)
 
